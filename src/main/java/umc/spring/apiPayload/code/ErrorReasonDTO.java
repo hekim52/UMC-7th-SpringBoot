67b9c25e2b99f534ef2334ce0f38cf84a50c4665
hekim52 <acrossnightsky@gmail.com>
2024-11-14 13:01:11 +0900

    feat: week 7 워크북 실습 완료

--- src/main/java/umc/spring/apiPayload/code/ErrorReasonDTO.java
+++ src/main/java/umc/spring/apiPayload/code/ErrorReasonDTO.java
@@ -14,7 +14,5 @@ public class ErrorReasonDTO {
     private final String code;
     private final String message;
 
-    public boolean getIsSuccess() {
-        return isSuccess;
-    }
+    public boolean getIsSuccess(){return isSuccess;}
 }
