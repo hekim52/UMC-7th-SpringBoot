4da3178a83acd5fa57fcf17678399328d76cf8ca
hekim52 <acrossnightsky@gmail.com>
2024-11-23 08:57:54 +0900

    feat: 실습 저장

--- src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -25,7 +25,9 @@ public enum ErrorStatus implements BaseErrorCode {
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
 
     // For test
-    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");;
+    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트"),
+
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "FOODCATEGORY4001", "음식 카테고리를 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
--- src/main/java/umc/spring/domain/Member.java
+++ src/main/java/umc/spring/domain/Member.java
@@ -2,21 +2,25 @@ package umc.spring.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.enums.Gender;
 import umc.spring.domain.enums.MemberStatus;
 import umc.spring.domain.mapping.MissionHistory;
-import umc.spring.domain.mapping.PreferFoodPick;
+import umc.spring.domain.mapping.MemberPrefer;
 import umc.spring.domain.mapping.SetNotifyHistory;
 import umc.spring.domain.mapping.TermsAgree;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -26,18 +30,11 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 20)
-    private String myId;
+//    @Column(nullable = false, length = 20)
+//    private String myId;
 
     @Column(nullable = false, length = 10)
     private String name;
-
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(15)")
-    private Gender gender;
-
-    @Column(nullable = false)
-    private LocalDate birth;
 
     @Column(nullable = false, length = 40)
     private String address;
@@ -45,32 +42,54 @@ public class Member extends BaseEntity {
     @Column(nullable = false, length = 40)
     private String specAddress;
 
-    @Column(nullable = false, length = 20)
-    private String nickname;
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15)")
+    private Gender gender;
 
-    @Column(nullable = false, length = 30)
-    private String email;
-
-    @Column(nullable = false, length = 20)
-    private String phoneNum;
-
-    @Column(nullable = false, length = 100)
-    private String picture;
+//    @Column(nullable = false)
+//    private LocalDate birth;
 
     @Column(nullable = false)
-    private Long clearMission;
+    private Integer birthYear;
 
     @Column(nullable = false)
-    private Long myPoint;
+    private Integer birthMonth;
+
+    @Column(nullable = false)
+    private Integer birthDay;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
     private MemberStatus status;
 
     private LocalDateTime inactiveDate;
 
+    @ColumnDefault("0")
+    private Long myPoint;
+
+//    @Enumerated(EnumType.STRING)
+//    private SocialType socialType;
+//
+//    @Column(nullable = false, length = 50)
+//    private String email;
+
+//    @Column(nullable = false, length = 20)
+//    private String nickname;
+
+//    @Column(nullable = false, length = 30)
+//    private String email;
+
+//    @Column(nullable = false, length = 20)
+//    private String phoneNum;
+//
+//    @Column(nullable = false, length = 100)
+//    private String picture;
+//
+//    @Column(nullable = false)
+//    private Long clearMission;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private List<PreferFoodPick> preferFoodPickList = new ArrayList<>();
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<SetNotifyHistory> setNotifyHistoryList = new ArrayList<>();
