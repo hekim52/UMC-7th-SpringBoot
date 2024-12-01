76c198f2a274c99de2f2f4cf9b32aae4171f3a6e
hekim52 <acrossnightsky@gmail.com>
2024-12-01 18:26:47 +0900

    featue: 내가 진행중인 미션 목록 조회 API 구현, 페이지 검증 어노테이션 구현 #16

--- src/main/java/umc/spring/repository/MissionHistoryRepository/MissionHistoryRepository.java
+++ src/main/java/umc/spring/repository/MissionHistoryRepository/MissionHistoryRepository.java
@@ -1,13 +1,19 @@
 package umc.spring.repository.MissionHistoryRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import umc.spring.domain.Mission;
 import umc.spring.domain.mapping.MissionHistory;
 
 import java.util.List;
 
 public interface MissionHistoryRepository extends JpaRepository<MissionHistory, Long> {
+
+    @Query("select mh.mission from MissionHistory mh where mh.member.id = :memberId and mh.missionState = 0")
+    Page<Mission> findNotFinishedMissionByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
     @Query("select mh from MissionHistory mh where mh.member.id = :memberId")
     List<MissionHistory> findAllByMemberId(@Param("memberId") Long memberId);
--- src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.spring.domain.Member;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
-import umc.spring.domain.ReviewReply;
 import umc.spring.repository.MemberRepository.MemberRepository;
+import umc.spring.repository.MissionHistoryRepository.MissionHistoryRepository;
 import umc.spring.repository.ReviewRepository.ReviewRepository;
 
 @Service
@@ -16,6 +17,7 @@ public class MemberQueryServiceImpl implements MemberQueryService {
 
     private final MemberRepository memberRepository;
     private final ReviewRepository reviewRepository;
+    private final MissionHistoryRepository missionHistoryRepository;
 
     @Override
     public Page<Review> getMyReviewList(Long memberId, Integer page) {
@@ -25,5 +27,13 @@ public class MemberQueryServiceImpl implements MemberQueryService {
         Page<Review> MemberPage = reviewRepository.findAllByMember(member, PageRequest.of(page, 10));
 
         return MemberPage;
+    }
+
+    @Override
+    public Page<Mission> getMyMissionList(Long memberId, Integer page) {
+
+        Page<Mission> MissionPage = missionHistoryRepository.findNotFinishedMissionByMemberId(memberId, PageRequest.of(page, 10));
+
+        return MissionPage;
     }
 }
