343cc961f3ffee304f324c5357779bef1331ee89
hekim52 <acrossnightsky@gmail.com>
2024-12-01 13:40:38 +0900

    featue: 가게의 리뷰 목록 조회 API  구현, 페이지 검증 어노테이션 구현 #6

--- src/main/java/umc/spring/validation/annotation/CheckPage.java
+++ src/main/java/umc/spring/validation/annotation/CheckPage.java
@@ -1,20 +1,20 @@
-//package umc.spring.validation.annotation;
-//
-//import jakarta.validation.Constraint;
-//import jakarta.validation.Payload;
-//import umc.spring.validation.validator.PageCheckValidator;
-//
-//import java.lang.annotation.*;
-//
-//@Documented
-//@Constraint(validatedBy = PageCheckValidator.class)
-//@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
-//@Retention(RetentionPolicy.RUNTIME)
-//public @interface CheckPage {
-//
-//    int value() default 1;
-//
-//    String message() default "페이지가 너무 작습니다.";
-//    Class<?>[] groups() default {};
-//    Class<? extends Payload>[] payload() default {};
-//}
+package umc.spring.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import umc.spring.validation.validator.PageCheckValidator;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PageCheckValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckPage {
+
+    int value() default 1;
+
+    String message() default "페이지가 0 이하입니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
--- src/main/java/umc/spring/web/controller/RestaurantRestController.java
+++ src/main/java/umc/spring/web/controller/RestaurantRestController.java
@@ -1,7 +1,14 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
@@ -11,7 +18,9 @@ import umc.spring.domain.Restaurant;
 import umc.spring.domain.Review;
 import umc.spring.domain.mapping.MissionHistory;
 import umc.spring.service.RestaurantService.RestaurantCommandService;
-import umc.spring.validation.annotation.ExistReviewRestaurant;
+import umc.spring.service.RestaurantService.RestaurantQueryService;
+import umc.spring.validation.annotation.CheckPage;
+import umc.spring.validation.annotation.ExistRestaurant;
 import umc.spring.web.dto.RestaurantRequestDTO;
 import umc.spring.web.dto.RestaurantResponseDTO;
 
@@ -22,9 +31,10 @@ import umc.spring.web.dto.RestaurantResponseDTO;
 public class RestaurantRestController {
 
     private final RestaurantCommandService restaurantCommandService;
+    private final RestaurantQueryService restaurantQueryService;
 
     // 가게 추가
-    @PostMapping("/add")
+    @PostMapping("/addRestaurant")
     public ApiResponse<RestaurantResponseDTO.AddRestaurantResultDTO> addRestaurant (
             @RequestBody @Valid RestaurantRequestDTO.AddRestaurantDTO request) {
 
@@ -36,7 +46,7 @@ public class RestaurantRestController {
     // 리뷰하기
     @PostMapping("/{restaurantId}/review")
     public ApiResponse<RestaurantResponseDTO.ReviewResultDTO> review (
-            @ExistReviewRestaurant @PathVariable Long restaurantId,
+            @ExistRestaurant @PathVariable Long restaurantId,
             @RequestBody @Valid RestaurantRequestDTO.ReviewDTO request) {
 
         Review review = restaurantCommandService.review(restaurantId, request);
@@ -65,5 +75,26 @@ public class RestaurantRestController {
         Mission mission = restaurantCommandService.addMission(restaurantId, request);
 
         return ApiResponse.onSuccess(RestaurantConverter.toAddMissionResultDTO(mission));
+    }
+
+    // 가게의 리뷰 목록 조회
+    @GetMapping("/{restaurantId}/reviews")
+    @Operation(summary = "특정 가게의 리뷰 목록 조회 API",description = "특정 가게의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "restaurantId", description = "가게의 아이디, path variable 입니다!")
+    })
+    public ApiResponse<RestaurantResponseDTO.ReviewPreViewListDTO> getReviewList(
+            @ExistRestaurant @PathVariable(name = "restaurantId") Long restaurantId,
+            @CheckPage @RequestParam(name = "page") Integer page){
+
+        Page<Review> reviewList = restaurantQueryService.getReviewList(restaurantId,page-1);
+
+        return ApiResponse.onSuccess(RestaurantConverter.reviewPreViewListDTO(reviewList));
     }
 }
