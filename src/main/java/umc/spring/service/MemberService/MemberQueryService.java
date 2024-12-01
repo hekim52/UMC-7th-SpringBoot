fc236584376de9894d36664f0c65e8c2d5233f4f
hekim52 <acrossnightsky@gmail.com>
2024-12-01 15:54:18 +0900

    featue: 내가 작성한 리뷰 목록 조회 API 구현, 페이지 검증 어노테이션 구현 #14

--- src/main/java/umc/spring/service/MemberService/MemberQueryService.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryService.java
@@ -1,4 +1,10 @@
 package umc.spring.service.MemberService;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
+import umc.spring.domain.ReviewReply;
+
 public interface MemberQueryService {
+
+    Page<Review> getMyReviewList(Long memberId, Integer page);
 }
