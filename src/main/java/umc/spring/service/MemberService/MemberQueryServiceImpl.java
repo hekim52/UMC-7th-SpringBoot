54b35a20dcc6a6055ae10b9346812639ca161552
hekim52 <acrossnightsky@gmail.com>
2024-12-01 15:54:18 +0900

    featue: 내가 작성한 리뷰 목록 조회 API 구현, 페이지 검증 어노테이션 구현 #14

--- src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
@@ -1,9 +1,29 @@
 package umc.spring.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import umc.spring.domain.Member;
+import umc.spring.domain.Review;
+import umc.spring.domain.ReviewReply;
+import umc.spring.repository.MemberRepository.MemberRepository;
+import umc.spring.repository.ReviewRepository.ReviewRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MemberQueryServiceImpl implements MemberQueryService {
+
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public Page<Review> getMyReviewList(Long memberId, Integer page) {
+
+        Member member = memberRepository.findById(memberId).get();
+
+        Page<Review> MemberPage = reviewRepository.findAllByMember(member, PageRequest.of(page, 10));
+
+        return MemberPage;
+    }
 }
