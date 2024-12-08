6cdb58006af2366b257cd7231472a1aeb7ff29e4
hekim52 <acrossnightsky@gmail.com>
2024-12-08 14:59:36 +0900

    feature: Spring Security를 활용한 로그인 및 회원가입 구현 #22

--- src/main/java/umc/spring/domain/Member.java
+++ src/main/java/umc/spring/domain/Member.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.enums.Gender;
 import umc.spring.domain.enums.MemberStatus;
+import umc.spring.domain.enums.Role;
 import umc.spring.domain.mapping.MissionHistory;
 import umc.spring.domain.mapping.MemberPrefer;
 import umc.spring.domain.mapping.SetNotifyHistory;
@@ -113,7 +114,20 @@ public class Member extends BaseEntity {
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<MissionHistory> missionHistoryList = new ArrayList<>();
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     public void setMissionHistory(MissionHistory missionHistory) {
         this.missionHistoryList.add(missionHistory);
+    }
+
+    public void encodePassword(String password) {
+        this.password = password;
     }
 }
