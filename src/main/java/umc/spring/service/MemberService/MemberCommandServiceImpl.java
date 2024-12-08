943992279d83997b816abe5b44f6e4cb0c47b276
hekim52 <acrossnightsky@gmail.com>
2024-12-08 14:59:36 +0900

    feature: Spring Security를 활용한 로그인 및 회원가입 구현 #22

--- src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
@@ -2,6 +2,7 @@ package umc.spring.service.MemberService;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.apiPayload.code.status.ErrorStatus;
@@ -27,15 +28,18 @@ import java.util.stream.Collectors;
 public class MemberCommandServiceImpl implements MemberCommandService {
 
     private final MemberRepository memberRepository;
-
     private final FoodCategoryRepository foodCategoryRepository;
     private final MissionHistoryRepository missionHistoryRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public Member joinMember(MemberRequestDTO.JoinDto request) {
 
         Member newMember = MemberConverter.toMember(request);
+        newMember.encodePassword(passwordEncoder.encode(request.getPassword()));
+
         List<FoodCategory> foodCategoryList = request.getPreferCategory().stream()
                 .map(category -> {
                     return foodCategoryRepository.findById(category).orElseThrow(() -> new FoodCategoryHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
